b6843fe5fd4195f2caa29f84ddc69321b65af0d2
Érick Santos <ericksantos@mailfence.com>
2024-01-19 22:49:54 -0300

    Adds TournamentView Model

---
+++ Models/TournamentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace api_mvc.Models
+{
+    public class TournamentViewModel
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsFinished { get; set; }
+        public bool IsStarted { get; set; }
+        public int PlayersNumber { get; set; }
+        public int RoundsNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
